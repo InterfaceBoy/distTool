bc22eb8c02d2af69a8aca09c1a7078a043fb30db
heyuanpeng <991800152@qq.com>
2020-11-18 09:12:00 +0800

    fix(公共模块): 修改了第一界面

--- src/views/home-page.vue
+++ src/views/home-page.vue
@@ -1,8 +1,12 @@
 <template>
-  <!-- <div class="home-page">
-    <Menu active-name="1-2" :open-names="['1']" on-select="handleSelect"></Menu>
-  </div >-->
-  <div class="home-page">1234</div>
+  <div class="home-page">
+    <Menu
+      active-name="1-2"
+      :open-names="['1', '2', '3']"
+      on-select="handleSelect"
+    ></Menu>
+  </div>
+  <!-- <div class="home-page">1234</div> -->
 </template>
 
 <script>
